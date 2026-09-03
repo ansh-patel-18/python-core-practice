1b758b81a412dbab4de8804727cf17f5e347f639
ansh-patel-18 <anshupatel5753@gmail.com>
2026-09-03 09:53:15 +0530

    oop class subject is clear

---
+++ PRACTICE/09_OOP_function.py
@@ -0,0 +1,36 @@
+class real:
+    language = "py"
+    salary = 50000
+
+harry = real()
+harry.name = "Ansh"
+print(harry.name, harry.language, harry.salary)
+
+rohan = real()
+rohan.name = "Rohan"
+print(rohan.language,rohan.name, rohan.salary)
+
+###########################################################################################
+
+# class student:
+#     pass
+# s1=student()
+
+# s1.name="Ansh"
+# s1.age = "21"
+
+# print(s1.name)
+# print(s1.age)
+# print(type(s1))
+
+###########################################################################################
+
+# class Product:
+#     def __init__(self, title:str, price:int):
+#         self.title = title
+#         self.price = price
+
+# p1 = Product(title="Leptop", price=50000)
+
+# print(p1.title)
+# print(p1.price)
