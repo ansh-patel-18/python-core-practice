798ddb08d5e0e6a810ff05769f8d3bd35bc251da
ansh-patel-18 <anshupatel5753@gmail.com>
2026-09-05 19:07:32 +0530

    Update 09_OOP_function.py

--- PRACTICE/09_OOP_function.py
+++ PRACTICE/09_OOP_function.py
@@ -1,14 +1,14 @@
-class real:
-    language = "py"
-    salary = 50000
+# class real:
+#     language = "py"
+#     salary = 50000
 
-harry = real()
-harry.name = "Ansh"
-print(harry.name, harry.language, harry.salary)
+# harry = real()
+# harry.name = "Ansh"
+# print(harry.name, harry.language, harry.salary)
 
-rohan = real()
-rohan.name = "Rohan"
-print(rohan.language,rohan.name, rohan.salary)
+# rohan = real()
+# rohan.name = "Rohan"
+# print(rohan.language,rohan.name, rohan.salary)
 
 ###########################################################################################
 
@@ -34,3 +34,28 @@ print(rohan.language,rohan.name, rohan.salary)
 
 # print(p1.title)
 # print(p1.price)
+
+##################################################################
+
+class Employee:
+    language = "py"
+    salary = 20000
+
+e1 = Employee()
+e1.name = "Ansh"
+e1.language = "java"
+print("\nName \t\t\t: ",e1.name)
+print("Ansh Coding Language \t: ",e1.language)
+print("Ansh Salary \t\t: ",e1.salary)
+
+
+e2 = Employee()
+e2.name = "Jeel"
+e2.language = "python"
+print("\nName \t\t: ",e2.name)
+print("Coding Language : ",e2.language)
+print("Salary \t\t: ",e2.salary)
+
+e3 = Employee()
+e3.data="He is the real manager"
+print(e3.data)
